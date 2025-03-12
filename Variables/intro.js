e6b54fbd95f1f63b4a42e5400302fb74441f22ce
Rishabh Singh 2004 <rishabhsinghsengar02gmail.com>
2025-03-13 00:09:10 +0530

    Commit

--- Variables/intro.js
+++ Variables/intro.js
@@ -3,5 +3,9 @@ var a = 21 // a contains 21
 console.log(a)
 a= "Rishabh Singh"
 console.log(a)
+b= "Upcoming"
+console.log(b)
+let c = 22
+console.log(c)
 // let 6rishabh = 7 //Not allowed this will show an error bcuz variable name should begin from "A letter or $ or underscore(_)"
 //let var =5 // Not allow bcuz reserverd keyword variable name is there
