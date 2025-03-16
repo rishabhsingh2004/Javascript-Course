5c69357d99c37a3e5e15d5bd90921249797f8002
Rishabh Singh 2004 <rishabhsinghsengar02gmail.com>
2025-03-16 21:08:07 +0530

    Commit

---
+++ Let,c onst , var/script.js
@@ -0,0 +1,30 @@
+//console.log("JavaScript Course: var, let and const")
+
+//-------------------------Var------------------------
+ var a = 45;
+var a = 10;
+var a= "Rishabh"
+//var a = null
+//var a = undefined
+console.log(a)
+
+
+//-----------------------Let-------------------------
+//let b =4;
+//b= "Rishabh Singh"
+//let b = 10 // not allow bcuz they cannot be redeclared 
+//let c= null
+////let d =undefined
+//{
+//    let b = "Krishna";
+//   let c = 21;
+//    console.log(b)
+//   console.log(c)
+//}
+//console.log(b)
+
+
+//-----------------------Const-----------------------
+//const king= "Rishabh";
+//console.log(king)
+//a=80;// Show an error bcuz const value is constanst
