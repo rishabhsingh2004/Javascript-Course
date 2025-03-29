be702dd612f164095db3dfc10e7e2860e8d25c80
Rishabh Singh 2004 <rishabhsinghsengar02gmail.com>
2025-03-29 23:55:35 +0530

    Commit

---
+++ Arrays/array.js
@@ -0,0 +1,13 @@
+let marks_of_class_12 = [ 58 , 80 , 92 , 78 , null , "Not present "]
+console.log(marks_of_class_12)
+console.log(marks_of_class_12[0])
+console.log(marks_of_class_12[1])
+console.log(marks_of_class_12[2])
+console.log(marks_of_class_12[3])
+console.log(marks_of_class_12[4])
+console.log(marks_of_class_12[5])
+console.log("The length of marks of class 12th are ",marks_of_class_12.length)
+
+
+
+
