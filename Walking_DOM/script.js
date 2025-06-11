dac663d54a5f6eb2f38a341a9887734d6200f8b2
Rishabh Singh 2004 <rishabhsinghsengar02gmail.com>
2025-06-11 14:03:22 +0530

    Commit

---
+++ Walking_DOM/script.js
@@ -0,0 +1,4 @@
+// Walking the document object model (DOM) in JavaScript
+// It is a way for JavaScript to interact with HTML and CSS on a web page.
+// The DOM represents the document as a tree structure, where each node is an object representing a part of the document.
+
